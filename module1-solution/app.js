76f42e97dc13ef837f4056dc61d065b126dbc338
pushkarshinde79 <pushkar.shinde79@gmail.com>
2020-05-25 09:47:12 +0530

    'module1-solution'

---
+++ module1-solution/app.js
@@ -0,0 +1,45 @@
+(function(){
+'use strict'
+var x = "hello";
+var stringLength=0;
+
+angular.module('myFirstApp',[])
+
+.controller ('myFirstController',myFirstController);
+myFirstController.$inject = [$scope];
+function myFirstController($scope)
+{
+
+   $scope.name = "";
+   $scope.totalLength=0;
+   $scope.msg = "";
+
+   $scope.displayLength = function(){
+     var totalStringLength = calculateStringLength($scope.name);
+     $scope.totalLength = totalStringLength;
+     if($scope.totalLength <= 3)
+     {
+       $scope.msg = "Enjoy !";
+     }
+     if($scope.totalLength > 3)
+     {
+       $scope.msg = "Too much !";
+     }
+
+
+
+ function calculateStringLength(string){
+      var tokens = string.split(',');
+      stringLength = tokens.length;
+      if(stringLength == 1 && tokens[0] == "")
+      {
+          stringLength--;
+      }
+    return stringLength;
+
+  }
+}
+}
+
+
+})();
