1850728ea892ad2eb531a300c33fe2f06a616718
pushkarshinde79 <pushkar.shinde79@gmail.com>
2020-05-26 16:34:25 +0530

    Next example

--- app.js
+++ app.js
@@ -1,40 +1,31 @@
-(function(){
-//'use strict'
-//var x = "hello";
-//var stringLength=0;
+(function () {
+'use strict';
 
-angular.module('myFirstApp',[])
+angular.module('DIApp', [])
+.controller('DIController', DIController);
 
-.controller ('myFirstController',[myFirstController]);
-myFirstController.$inject = [$scope];
-function myFirstController($scope)
-{
+DIController.$inject = ['$scope', '$filter'];
+function DIController($scope, $filter) {
+  $scope.name = "";
+  $scope.totalLength=0;
+  $scope.msg = "";
 
-   $scope.name = "";
-   $scope.totalLength=0;
-   $scope.msg = "";
+  $scope.displayLength = function(){
+    var totalStringLength = 1 ;calculateStringLength($scope.name);
+    $scope.totalLength = totalStringLength;
+    if($scope.totalLength <= 3)
+    {
+      $scope.msg = "Enjoy !";
+    }
+    if($scope.totalLength > 3)
+    {
+      $scope.msg = "Too much !";
+    }
 
-   // $scope.displayLength = function(){
-   //   var totalStringLength = 1; //calculateStringLength($scope.name);
-   //   $scope.totalLength = totalStringLength;
-   //   if($scope.totalLength <= 3)
-   //   {
-   //     $scope.msg = "Enjoy !";
-   //   }
-   //   if($scope.totalLength > 3)
-   //   {
-   //     $scope.msg = "Too much !";
-   //   }
-   // };
- //function calculateStringLength(string){
-//      var tokens = string.split(',');
-  //    stringLength = tokens.length;
-    //  if(stringLength == 1 && tokens[0] == "")
-      //{
-        //  stringLength--;
-    //  }
-  //  return stringLength;
-
-  //};
+  $scope.upper = function () {
+    var upCase = $filter('uppercase');
+    $scope.name = upCase($scope.name);
+  };
 }
+
 })();
