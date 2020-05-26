18c992a35bf6068a9790d8d0427c5c75bdc39b8b
pushkarshinde79 <pushkar.shinde79@gmail.com>
2020-05-26 17:44:01 +0530

    Next example

--- module1-solution/app.js
+++ module1-solution/app.js
@@ -1,45 +1,41 @@
-(function(){
-'use strict'
-var x = "hello";
+(function () {
+'use strict';
+
 var stringLength=0;
 
-angular.module('myFirstApp',[])
+angular.module('DIApp', [])
+.controller('DIController', DIController);
 
-.controller ('myFirstController',myFirstController);
-myFirstController.$inject = [$scope];
-function myFirstController($scope)
-{
+DIController.$inject = ['$scope', '$filter'];
+function DIController($scope, $filter) {
+  $scope.name = "abc";
+  $scope.totalLength=0;
+  $scope.msg = "4";
 
-   $scope.name = "";
-   $scope.totalLength=0;
-   $scope.msg = "";
-
-   $scope.displayLength = function(){
-     var totalStringLength = calculateStringLength($scope.name);
-     $scope.totalLength = totalStringLength;
-     if($scope.totalLength <= 3)
-     {
-       $scope.msg = "Enjoy !";
-     }
-     if($scope.totalLength > 3)
-     {
-       $scope.msg = "Too much !";
-     }
-
-
-
- function calculateStringLength(string){
+  $scope.displayLength = function(){
+      var string = $scope.name;
       var tokens = string.split(',');
       stringLength = tokens.length;
       if(stringLength == 1 && tokens[0] == "")
       {
           stringLength--;
       }
-    return stringLength;
+      $scope.totalLength = stringLength;
 
-  }
+    if($scope.totalLength <= 3)
+    {
+      $scope.msg = "Enjoy !";
+    }
+    if($scope.totalLength > 3)
+    {
+      $scope.msg = "Too much !";
+    }
+  //  $scope.msg = "Hi";
+};
+  // $scope.upper = function () {
+  //   var upCase = $filter('uppercase');
+  //   $scope.name = upCase($scope.name);
+  // };
 }
-}
-
 
 })();
